343ccbb2ce3833c41f0625de61e45dc62ab58765
이병관 <lbg@sk.com>
2025-07-20 00:52:22 +0900

    fix: Correct log message for common header settings in SktAiClientConfig

--- src/main/java/com/skax/aiportal/client/sktai/config/SktAiClientConfig.java
+++ src/main/java/com/skax/aiportal/client/sktai/config/SktAiClientConfig.java
@@ -69,7 +69,7 @@ public class SktAiClientConfig {
             //-- LBG requestTemplate.header("Content-Type", "application/x-www-form-urlencoded");
             requestTemplate.header("User-Agent", "AXPORTAL-Backend/1.0");
             
-            log.debug("공통 헤더 설정 완료 - Accept: application/json, Content-Type: application/x-www-form-urlencoded");
+            log.debug("공통 헤더 설정 완료 - Accept: application/json); //-- LBG , Content-Type: application/x-www-form-urlencoded");
         };
     }
 
